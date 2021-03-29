b858ed127b94cd41a75c9b0d8df6b2017d238998
rvdw <rogercode@gmail.com>
2021-03-29 14:55:27 -0400

    ¨makefile¨

--- cmd.sh
+++ cmd.sh
@@ -2,9 +2,10 @@
 #../darknet/darknet detector train obj.data cfg/yolov3-tiny_3l.cfg -dont_show -mjpeg_port 8090 -map
 #../darknet/darknet detector train obj.data cfg/yolov3-tiny_3l.cfg backup/yolov3-tiny_3l_best.weights -dont_show -clear 1 -mjpeg_port 8090 -map
 
-#../darknet/darknet detector train obj.data cfg/yolov3-tiny_xnor.cfg -dont_show -map 
+
+../darknet/darknet detector train obj.data cfg/yolov3-tiny_3l_xnor.cfg -dont_show -mjpeg_port 8090 -map 
 
 ## test
 #../darknet/darknet detector map ./obj.data ./cfg/yolov3-tiny_3l.cfg ../pretrained/yolov3-tiny_3l_best_firstrun.weights
-../darknet/darknet detector map ./obj.data ./cfg/yolov3-tiny_3l.cfg backup/yolov3-tiny_3l_best.weights
+#../darknet/darknet detector map ./obj.data ./cfg/yolov3-tiny_3l.cfg backup/yolov3-tiny_3l_best.weights
 #../darknet/darknet detector map ./obj.data ./cfg/yolov3-tiny_3l.cfg backup/tyv3_94-91.weights
